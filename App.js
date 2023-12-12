6865110853f8f30da38cf977e463f692355d392f
Makktu <johnmcna@gmail.com>
2023-12-12 03:50:00 +0000

    just messing

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light' />
+      <View className='flex-1 items-center justify-center bg-black'>
+        <View className='rounded-2xl items-center justify-center bg-slate-400  w-90 h-20'>
+          <Text className='text-center text-xl'>
+            Open up App.js to start working on your app!
+          </Text>
+        </View>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

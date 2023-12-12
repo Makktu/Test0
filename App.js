9cd1e81cf8b26ce0989ebfdbac8bb7e8ad741ce4
Makktu <johnmcna@gmail.com>
2023-12-12 03:54:48 +0000

    changes

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <View className='flex-1 items-center justify-center bg-black'>
-        <View className='rounded-2xl items-center justify-center bg-slate-400  w-90 h-20'>
+      <View className='flex-1 items-center justify-center bg-emerald-900'>
+        <View className='rounded-2xl items-center justify-center bg-emerald-400  w-90 h-30 p-5'>
           <Text className='text-center text-xl'>
-            Open up App.js to start working on your app!
+            Open up App.js{'\n'}to start working{'\n'}on your app!
           </Text>
         </View>
       </View>
